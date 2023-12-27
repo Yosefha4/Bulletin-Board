8b60ec714fd7eebc5669c6399657ac6eb0371df7
yosefha4 <yosefha4@ac.sce.ac.il>
2023-12-27 13:43:00 +0200

    Add TimeAgo & ReactionsEmoji (posts)

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,14 +1,26 @@
 /* eslint-disable no-unused-vars */
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h2>{post.title}</h2>
       <p>{post.content}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post}/>
     </article>
   ));
   return (
---
+++ src/features/posts/ReactionButtons.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postSlice";
+
+const reactionEmoji = {
+  thumbsUp: "👍",
+  wow: "😮",
+  heart: "❤️",
+  rocket: "🚀",
+
+};
+
+const ReactionButtons = ({ post }) => {
+  const dispatch = useDispatch();
+
+  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    return (
+      <button
+        key={name}
+        type="button"
+        className="reactionBtn"
+        onClick={() =>
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+      >
+        {emoji} {post.reactions[name]}
+      </button>
+    );
+  });
+  return <div style={{display:'flex' , justifyContent:'space-between'}}>{reactionButtons}</div>;
+};
+
+export default ReactionButtons;
